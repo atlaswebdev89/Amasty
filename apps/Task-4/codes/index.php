0c058ea5a76e181375eab6aebb5562ecd57629a1
Atlas_WebDev <web.dev89@yandex.by>
2021-03-20 15:16:00 +0300

    start task-4

---
+++ apps/Task-4/codes/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Результаты чемпианотов Италия Серия А</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <section class="mt-5">
+            <div class="container">
+                <div class="row no-gutters justify-content-center">
+                    <h1>Футбок Италия Серия А</h1>
+                </div>
+                <div class="row justify-content-center content">
+                        <form id="main-form">
+                            <div class="form-group ">
+                                <label for="rated">Укажите название команды...</label>
+                                <input type="text" class="form-control" id="rated" name="rated" placeholder="Укажите название команды...">
+                            </div>
+                            <div class="text-right">
+                                <button type="submit" class="btn btn-primary">Отправить</button>
+                            </div>
+                        </form>
+                </div>
+            </div>
+        </section>
+        
+        <script  src="js/jquery-3.2.1.min.js"></script>
+        <script  src="js/bootstrap.min.js"></script>
+        <script  src="js/jquery-validation.min.js"></script>
+        <script  src="js/main.js"></script>
+    </body>
+</html>
+
---
+++ apps/Task-4/codes/js/main.js
@@ -0,0 +1,102 @@
+(function ($) {
+     "use strict";
+     
+        /* Изменение поведения placeholder при событиях фокусировке и потере фокуса */
+            $('form').find('input, textarea').each(function (e) {
+                    let input= $(this);
+                    let placeholder =input.attr('placeholder');
+                    input.on('focus', function (e) {
+                            $(this).attr('placeholder','');
+                    })
+                    input.on('blur', function (e) {
+                            $(this).attr('placeholder',placeholder);
+                    });
+            });
+        
+        /* Валидация данных формы перед выполнением ajax запроса*/
+	$('#main-form').validate({
+		rules: {
+			rated:{
+				required:true,
+				minlength: 2
+			},
+			summa:{
+                                minlength: 1,
+				required:true,
+                                digits: true
+			},
+			
+		},
+		errorPlacement: function (error, element) {},
+		submitHandler: function(form) {
+			urlencodeForm(form);
+		}
+	});
+        
+        function urlencodeForm(form) {
+		let form_encode = $(form).serializeArray();
+		ajaxTransferUrlEncode(form, form_encode);
+	}
+        
+        /*Функция передачи данных формы*/
+	function ajaxTransferUrlEncode(forma, dataForm) {
+		
+                let uri = 'scripts/bankomat.php';
+                let form =$(forma);
+                let elem = form.closest('div');
+            
+                
+		
+                $.ajax({
+			type: 'POST',
+			url: uri,
+			data:dataForm,
+			timeout: 5000,
+			//Указывая тип json использовать функцию JSON.parse не надо будет ошибка
+			dataType: "json",
+			beforeSend: function (data) {
+				//Блокируем кнопку и элементы формы
+				form.find('button, input, textarea').attr('disabled', 'disabled');
+			},
+			success:  function (data) {
+				if(data) {
+                                        //Убираем блок вывода
+                                        $('.msg').remove();
+					//Если ошибок нет, очищаем форму
+					if(data.status == true){
+						//Включение кнопки и элементов формы
+						form.find('button, input, textarea').removeAttr('disabled');
+                                                    //Формируем полученный результат
+                                                    elem.append("<div class='msg col-md-12 mt-3'><p class='m-3 '>Ответ:</p></div>");
+                                                    let content = '<table class="table table-striped">';
+                                                    content+='<thead><tr><th>Номинал</th><th>Количество</th></tr></thead>';
+                                                    content+='<tbody>';
+                                                        for(var key in data.message){
+                                                            content+='<tr><td>'+key+'</td><td>'+data.message[key]+'</td></tr>';
+                                                        }
+                                                    content+='</tbody></table>';
+                                                $('.msg').append(content);                                                                
+					}else if(data.status == false) {
+						//Включение кнопки и элементов формы
+						form.find('button, input, textarea').removeAttr('disabled');
+						//Формируем полученный результат
+                                                elem.append("<div class='msg col-md-12 mt-3'><p class='m-3 '>Ответ:</p></div>");
+						let content = '<div class="m-3 p-3 error-msg"><p class="m-0">'+data.message+'</p><div>';
+						$('.msg').append(content);
+					}
+				}
+			},
+			error: function(x, t, e){
+				if( t === 'timeout') {
+					//Включение кнопки и элементов формы
+					form.find('button,input, textarea').removeAttr('disabled');
+                                        //Формируем полученный результат
+                                                elem.append("<div class='msg col-md-12 mt-3'><p class='m-3 '>Ответ:</p></div>");
+						let content = '<div class="m-3 p-3 error-msg"><p>Превышено время ожидания</p><div>';
+						$('.msg').append(content);
+				}
+			}
+		})
+	}
+    
+})(jQuery);
