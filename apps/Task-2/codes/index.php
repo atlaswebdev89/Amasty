ec2bbb83817df6ee615fe4fc4e2134b6d80eecfd
Atlas_WebDev <web.dev89@yandex.by>
2021-03-21 21:39:49 +0300

    final

--- apps/Task-2/codes/index.php
+++ apps/Task-2/codes/index.php
@@ -1,8 +1,37 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$slova = [
+            'red'       =>      '#FF0000',
+            'blue'      =>      '#0000FF',
+            'green'     =>      '#008000',
+            'yellow'    =>      '#FFFF00',
+            'lime'      =>      '#00FF00',
+            'magenta'   =>      '#FF00FF',
+            'black'     =>      '#000000',
+            'gold'      =>      '#FFD700',
+            'gray'      =>      '#808080',
+            'tomato'    =>      '#FF6347',
+         ];
+        
+        echo '<div>';
+            $string =1; 
+            while($string < 6){
+                $keys = array_rand($slova, 5);  
+                echo '<p style=" font-size:30px;">';
+                   foreach ($keys as $key) {
+                       $item_color = $slova[$key];
+                       //Удаляем текущий цвет из массива
+                       unset($slova[$key]);
+                       //Определяем рандомно цвет 
+                       $color = $slova[array_rand($slova, 1)];
+                       
+                            echo '<span style="color:'.$color.';"> ' .$key. ' </span>';
+                       
+                       //Возвращаем в массив цвет 
+                       $slova[$key] = $item_color;
+                   }
+                $string++;
+            }
+        echo '</div>'; 
+       
+         
